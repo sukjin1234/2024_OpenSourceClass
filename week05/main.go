8adf12329024c6808d2fa7014ae0e79abf71cbdc
sukjin1234 <tjrwls5661@naver.com>
2024-09-27 12:07:30 +0900

    v0.2 입출력

--- week05/main.go
+++ week05/main.go
@@ -1,9 +1,14 @@
 package main
 
-import "fmt"
+import "fmt" //입출력 관련 라이브러리
 
 func main() {
-	var i int = 55
+	// var i int = 55
+	// var i int
+	// i = 55
+
+	i := 55 // :=  뒤에 오는 값의 자료형을 유추해서 변수에 담아줌
+
 	fmt.Printf("value i : %d\n", i)
 	fmt.Print("value i : ", i, "\n")
 	fmt.Println("value i :", i)
