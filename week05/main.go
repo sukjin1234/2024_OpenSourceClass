83cd3a5772641e022cc91313aecd818580c7c3ef
sukjin1234 <tjrwls5661@naver.com>
2024-09-27 12:24:03 +0900

    v0.1 출력 및 다양한 라이브러리

--- week05/main.go
+++ week05/main.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt" //입출력 관련 라이브러리
+import (
+	"fmt" //입출력 관련 라이브러리
+	"math"
+	"reflect"
+	"strings"
+)
 
 func main() {
 	// var i int = 55
@@ -8,6 +13,14 @@ func main() {
 	// i = 55
 
 	i := 55 // :=  뒤에 오는 값의 자료형을 유추해서 변수에 담아줌
+
+	var f float32 = 4.30
+	//f := 4.30 //float64 에 할당됨
+
+	fmt.Println(reflect.TypeOf(f), reflect.TypeOf(i))
+
+	fmt.Printf("%s\n", strings.Title("inhatc class"))
+	fmt.Println(math.Ceil(3.99))
 
 	fmt.Printf("value i : %d\n", i)
 	fmt.Print("value i : ", i, "\n")
