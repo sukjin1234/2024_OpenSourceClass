0af109cb58ce1cb5a8945d4a11ac96ddd3ffbb08
sukjin1234 <tjrwls5661@naver.com>
2024-10-04 10:39:36 +0900

    v0.5

--- week06/main.go
+++ week06/main.go
@@ -8,11 +8,12 @@ import (
 func main() {
 
 	i := 13 // :=  단축연산자 ,뒤에 오는 값의 자료형을 유추해서 변수에 담아줌
-	f := 12.9
+	var f float32 = 12.9
+
 	fmt.Printf("value i : %d, value f : %f\n", i, f)
 	//fmt.Printf("%d * %f = %d\n", i, f, i*int(f))
 	//fmt.Printf("%d * %f = %d\n", i, f, float(i)*f)
-	fmt.Println(reflect.TypeOf(i))
+	fmt.Println(reflect.TypeOf(i), reflect.TypeOf(f))
 	//go 언어는 같은 자료형끼리만 연산가능
 
 }
