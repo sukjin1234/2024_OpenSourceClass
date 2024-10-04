af6a49962b6b409ecd1cb80d81cfca63c596c77e
sukjin1234 <tjrwls5661@naver.com>
2024-10-04 11:24:37 +0900

    v0.7 zero value

--- week06/main.go
+++ week06/main.go
@@ -1,21 +1,18 @@
 package main
 
 import (
-	"fmt" //입출력 관련 라이브러리
-	"reflect"
+	"fmt"
 )
 
 func main() {
+	var f float64
+	var i int  // zero value
+	var b bool //선언 후 자동 초기화
+	var s string
 
-	i := 13 // :=  단축연산자 ,뒤에 오는 값의 자료형을 유추해서 변수에 담아줌
-	var f float32 = 12.9
-	c1 := 'A' //go의 문자변수는 크기가 가변적임
-	c2 := '김'
-	fmt.Printf("value i : %d, value f : %f\n", i, f)
-	//fmt.Printf("%d * %f = %d\n", i, f, i*int(f))
-	//fmt.Printf("%d * %f = %d\n", i, f, float(i)*f)
-	fmt.Println(c1, c2)
-	fmt.Println(reflect.TypeOf(i), reflect.TypeOf(f), reflect.TypeOf(c1), reflect.TypeOf(c2))
-	//go 언어는 같은 자료형끼리만 연산가능
-
+	fmt.Println(f, b, s, i) //문자열값이 없으므로 출력 X
+	fmt.Printf("%f %t %s %d\n", f, b, s, i)
+	f = 2.7
+	i = 3
+	fmt.Print(f > float64(i), "\n") //comparsion (true/false)
 }
