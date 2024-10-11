4e366b2093aa2be728031dee2959ccb6f647b5a7
sukjin1234 <tjrwls5661@naver.com>
2024-10-11 11:53:38 +0900

    v1.4 ParseInt

--- week07/main.go
+++ week07/main.go
@@ -5,17 +5,24 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
-	input := bufio.NewReader(os.Stdin)
-	fmt.Print("input your name:")
-	name, err := input.ReadString('\n')
-	//name, _ := input.ReadString('\n')
-
+	fmt.Print("input your score : ")
+	r := bufio.NewReader(os.Stdin)
+	i, err := r.ReadString('\n')
 	if err != nil {
 		log.Fatal(err)
+	}
+
+	i = strings.TrimSpace(i)                  // python strip
+	score, err := strconv.ParseInt(i, 16, 32) // 문자열 -> 정수 (변환할 변수, 진수크기, 비트크기)
+	if score >= 60 {
+		fmt.Printf("%d\n", score)
+		fmt.Println("A")
 	} else {
-		fmt.Print(name)
+		fmt.Println("BCDF")
 	}
 }
