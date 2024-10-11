4dd3a1d70cfae9becdc6e511076111beddf27efa
sukjin1234 <tjrwls5661@naver.com>
2024-10-11 10:42:38 +0900

    ver1.1 NewReplacer

--- week07/main.go
+++ week07/main.go
@@ -2,15 +2,11 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"strings"
 )
 
 func main() {
-	var now time.Time = time.Now()
-	var year int = now.Year()
-	var month int = int(now.Month())
-	var day int = now.Day()
-	fmt.Printf("오늘은 %d년 %d월 %d일 입니다.\n", year, month, day)
-	fmt.Printf("지금은 %d시 %d분 %d초 입니다.\n", now.Hour(), now.Minute(), now.Second())
-	fmt.Println(now.Month())
+	var broken string = "1 . 2 . 3 . 4"
+	r := strings.NewReplacer(".", ",")
+	fmt.Println(r.Replace(broken))
 }
