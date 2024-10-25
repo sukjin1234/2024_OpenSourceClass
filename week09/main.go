6a4463911d81993dc43f5cfce755b805b94d2a7b
sukjin1234 <tjrwls5661@naver.com>
2024-10-25 10:42:27 +0900

    v1.7 guess gaim

--- week09/main.go
+++ week09/main.go
@@ -1,13 +1,40 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
 	"math/rand"
+	"os"
+	"strconv"
+	"strings"
 	"time"
 )
 
 func main() {
 	rand.Seed(time.Now().Unix())
-	target := rand.Intn(100) + 1 //rand.Intn(0~n-1)
-	fmt.Printf("%d", target)
+	answer := rand.Intn(100) + 1 //rand.Intn(0~n-1)
+	fmt.Printf("%d\n", answer)
+
+	fmt.Print("input your score : ")
+	r := bufio.NewReader(os.Stdin)
+	i, err := r.ReadString('\n')
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	i = strings.TrimSpace(i)
+	score, err := strconv.Atoi(i)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(score)
+
+	if answer == score {
+		fmt.Print("정답")
+	} else if answer > score {
+		fmt.Print("Low")
+	} else if answer < score {
+		fmt.Print("High")
+	}
 }
