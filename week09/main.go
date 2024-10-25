9b7ccbcc5234fe67fff1407e192bf0c2d91c7bbc
sukjin1234 <tjrwls5661@naver.com>
2024-10-25 11:24:00 +0900

    v1.9 guess game final

--- week09/main.go
+++ week09/main.go
@@ -12,10 +12,11 @@ import (
 )
 
 func main() {
+
 	rand.Seed(time.Now().Unix())
 	answer := rand.Intn(100) + 1 //rand.Intn(0~n-1)
 	fmt.Printf("%d\n", answer)
-
+	win := false
 	for guesses := 0; guesses < 3; guesses++ {
 		fmt.Printf("%d번 남음, input your score : ", 3-guesses)
 		r := bufio.NewReader(os.Stdin)
@@ -33,11 +34,18 @@ func main() {
 
 		if answer == score {
 			fmt.Println("정답")
+			win = true
+			break
 		} else if answer > score {
 			fmt.Println("Low")
 		} else if answer < score {
 			fmt.Println("High")
 		}
-	}
 
+	}
+	if win {
+		fmt.Println("Win")
+	} else {
+		fmt.Println("Lose")
+	}
 }
