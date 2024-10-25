c5fc7a4c25ea75d83dd23ecca319b2c6e32aaed8
sukjin1234 <tjrwls5661@naver.com>
2024-10-25 11:11:39 +0900

    v1.8 git game

--- week09/main.go
+++ week09/main.go
@@ -16,25 +16,28 @@ func main() {
 	answer := rand.Intn(100) + 1 //rand.Intn(0~n-1)
 	fmt.Printf("%d\n", answer)
 
-	fmt.Print("input your score : ")
-	r := bufio.NewReader(os.Stdin)
-	i, err := r.ReadString('\n')
-	if err != nil {
-		log.Fatal(err)
+	for guesses := 0; guesses < 3; guesses++ {
+		fmt.Printf("%d번 남음, input your score : ", 3-guesses)
+		r := bufio.NewReader(os.Stdin)
+		i, err := r.ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		i = strings.TrimSpace(i)
+		score, err := strconv.Atoi(i)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(score)
+
+		if answer == score {
+			fmt.Println("정답")
+		} else if answer > score {
+			fmt.Println("Low")
+		} else if answer < score {
+			fmt.Println("High")
+		}
 	}
 
-	i = strings.TrimSpace(i)
-	score, err := strconv.Atoi(i)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(score)
-
-	if answer == score {
-		fmt.Print("정답")
-	} else if answer > score {
-		fmt.Print("Low")
-	} else if answer < score {
-		fmt.Print("High")
-	}
 }
