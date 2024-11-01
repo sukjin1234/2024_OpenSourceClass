fa256d4cbd1a9fdc40ef3b52e188552699382f25
sukjin1234 <tjrwls5661@naver.com>
2024-11-01 10:51:21 +0900

    v2.4 Prime Number

--- week10/main.go
+++ week10/main.go
@@ -19,14 +19,16 @@ func main() {
 
 	i = strings.TrimSpace(i) // python strip
 	score, err := strconv.Atoi(i)
-	count := 0
+
+	//count := 0
+	var isPrime bool = true //가독성, 저장공간크기 개선
 	for j := 2; j < score; j++ {
 		if score%j == 0 {
-			count++
+			isPrime = false
 		}
 	}
 
-	if count == 0 {
+	if isPrime { // 비교 연산 제거
 		fmt.Printf("%d 소수", score)
 	} else {
 		fmt.Printf("%d 소수 X", score)
