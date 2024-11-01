4f05e6925c81ae644cdcbd3ef8f3493364106160
sukjin1234 <tjrwls5661@naver.com>
2024-11-01 10:55:54 +0900

    v2.5 Prime number

--- week10/main.go
+++ week10/main.go
@@ -22,9 +22,14 @@ func main() {
 
 	//count := 0
 	var isPrime bool = true //가독성, 저장공간크기 개선
-	for j := 2; j < score; j++ {
-		if score%j == 0 {
-			isPrime = false
+	// 논리적 오류 해결
+	if score < 2 { // 1 보다 큰 자연수중 1과 자신만을 약수로 가지는 수
+		isPrime = false
+	} else {
+		for j := 2; j < score; j++ {
+			if score%j == 0 {
+				isPrime = false
+			}
 		}
 	}
 
