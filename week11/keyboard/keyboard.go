d2094bc8c20e59c6b73fcc6998651d5c5b1eeb86
sukjin1234 <tjrwls5661@naver.com>
2024-11-08 11:45:10 +0900

    v3.4 keyboard package

---
+++ week11/keyboard/keyboard.go
@@ -0,0 +1,26 @@
+package keyboard
+
+import (
+	"bufio"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func GetInteger() (int, error) {
+	r := bufio.NewReader(os.Stdin)
+	a, err := r.ReadString('\n')
+	if err != nil {
+		log.Fatal(err)
+		return 0, err
+	}
+
+	a = strings.TrimSpace(a)
+	n, err := strconv.Atoi(a)
+	if err != nil {
+		log.Fatal(err)
+		return 0, err
+	}
+	return n, nil
+}
--- week11/main.go
+++ week11/main.go
@@ -1,8 +1,12 @@
 package main
 
 // package 이름과 package를 담고 있는 파일 이름이 같아야함
-import "main.go/greeting"
+import (
+	"main.go/greeting"
+	"main.go/keyboard"
+)
 
 func main() {
 	greeting.AllGreeting("ll")
+	keyboard.GetInteger()
 }
