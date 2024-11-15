7d44a835034df69af8f0745b5bfaf013b30dacaa
sukjin1234 <tjrwls5661@naver.com>
2024-11-15 10:51:56 +0900

    v3.7 Array

--- week12/main.go
+++ week12/main.go
@@ -18,5 +18,10 @@ func main() {
 		time.Unix(1747920000, 0),
 		time.Unix(1447920001, 1), // 쉼표가 반드시 필요
 	}
-	fmt.Print(dates[0], dates[1], dates[2])
+	// fmt.Println(dates[0], dates[1], dates[2])
+	// fmt.Printf("%#v", dates) // %#v Array literal 형태로 출력
+
+	for i := 0; i < len(dates); i++ {
+		fmt.Println(i, dates[i])
+	}
 }
