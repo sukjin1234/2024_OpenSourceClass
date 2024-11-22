329997a1cc51d3b5014a85316065589104f7549a
sukjin1234 <tjrwls5661@naver.com>
2024-11-22 11:39:17 +0900

    4.4 emptyslice

--- week13/main.go
+++ week13/main.go
@@ -5,8 +5,11 @@ import (
 )
 
 func main() {
-	var emptySlice []int
-	//emptySlice = make([]int, 10)
+	var emptySlice []int // slice can get nil data
+	//emptySlice = make([]int, 5) // []int{0,0,0,0,0}
+	if len(emptySlice) == 0 {
+		emptySlice = append(emptySlice, 1)
+	}
 	fmt.Printf("%#v\n", emptySlice)
 	var gpa [5]float64 = [5]float64{3.5, 4.1, 4.5, 4.2, 3.9}
 	gpaSlice := gpa[1:4] // 4.1, 4.5, 4.2
