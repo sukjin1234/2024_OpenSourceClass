136bcfa8af9a595410cbc80165fb8b94e013adec
sukjin1234 <tjrwls5661@naver.com>
2024-11-22 11:29:59 +0900

    4.3 nil slice

--- week13/main.go
+++ week13/main.go
@@ -5,6 +5,9 @@ import (
 )
 
 func main() {
+	var emptySlice []int
+	//emptySlice = make([]int, 10)
+	fmt.Printf("%#v\n", emptySlice)
 	var gpa [5]float64 = [5]float64{3.5, 4.1, 4.5, 4.2, 3.9}
 	gpaSlice := gpa[1:4] // 4.1, 4.5, 4.2
 	//gpaSlice[1] = 2.71
