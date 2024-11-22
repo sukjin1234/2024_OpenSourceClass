9e4cb796fcf78eaca637be817675ab3df9f86ced
sukjin1234 <tjrwls5661@naver.com>
2024-11-22 11:27:36 +0900

    4.2 slice

---
+++ week13/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var gpa [5]float64 = [5]float64{3.5, 4.1, 4.5, 4.2, 3.9}
+	gpaSlice := gpa[1:4] // 4.1, 4.5, 4.2
+	//gpaSlice[1] = 2.71
+	gpa[2] = 5
+	gpaSlice = append(gpaSlice, 9)
+	fmt.Println(gpaSlice, gpa)           // slice point array
+	fmt.Println(len(gpaSlice), len(gpa)) // slice point array
+}
