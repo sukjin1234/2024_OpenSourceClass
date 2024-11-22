37d4e32d0f9d8ae1e3270bba32fff8bde7b01758
sukjin1234 <tjrwls5661@naver.com>
2024-11-22 12:02:05 +0900

    4.4 variable parameter

--- week13/main.go
+++ week13/main.go
@@ -2,20 +2,23 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"reflect"
 )
 
+func test(strs ...string) { // ...string -> slice, like variable parameter in python
+	// variable parameter should locate last parameter
+	fmt.Println(strs, reflect.TypeOf(strs))
+}
+
 func main() {
-	var emptySlice []int // slice can get nil data
-	//emptySlice = make([]int, 5) // []int{0,0,0,0,0}
-	if len(emptySlice) == 0 {
-		emptySlice = append(emptySlice, 1)
-	}
-	fmt.Printf("%#v\n", emptySlice)
-	var gpa [5]float64 = [5]float64{3.5, 4.1, 4.5, 4.2, 3.9}
-	gpaSlice := gpa[1:4] // 4.1, 4.5, 4.2
-	//gpaSlice[1] = 2.71
-	gpa[2] = 5
-	gpaSlice = append(gpaSlice, 9)
-	fmt.Println(gpaSlice, gpa)           // slice point array
-	fmt.Println(len(gpaSlice), len(gpa)) // slice point array
+	slice := os.Args[1:] // only string
+	fmt.Println(slice[1])
+	fmt.Printf("%T\n", slice[1])
+	slice = append(slice, "a", "b", "c", "d") // can put many argument
+	fmt.Println(slice)
+	test("1", "2,3,4")
+	test("1")
+	test("1", "2,3,4", "5,6")
+	test()
 }
