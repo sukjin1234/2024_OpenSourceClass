f71adb9abd73bdf5272e6c6323f3ff7df9620b8d
sukjin1234 <tjrwls5661@naver.com>
2024-11-29 11:31:32 +0900

    4.6 struct type

--- week14/main.go
+++ week14/main.go
@@ -2,21 +2,19 @@ package main
 
 import "fmt"
 
+type student struct {
+	id   int
+	name string
+	gpa  float32
+}
+
 func main() {
-	var student1 struct {
-		id   int
-		name string
-		gpa  float32
-	}
+	var student1 student
 	student1.id = 20241234
 	student1.name = "whdo"
 	student1.gpa = 4.5
 	fmt.Printf("%d %s %f\n", student1.id, student1.name, student1.gpa)
-	var student2 struct {
-		id   int
-		name string
-		gpa  float32
-	}
+	var student2 student
 	student2.id = 20241234
 	student2.name = "dddd"
 	student2.gpa = 4.7
